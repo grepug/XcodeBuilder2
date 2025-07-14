52db4089708c9275bd82e9a8e64a2dd1af6c4de6
Kai Shao <grepug@icloud.com>
2025-07-14 11:31:42 +0800

    Add ProjectDetailView, ProjectEditorView, ProjectList, and ProjectListItemView for project management interface

---
+++ XcodeBuilder2/Screens/ProjectDetail/ProjectDetailView.swift
@@ -0,0 +1,16 @@
+//
+//  ProjectDetailView.swift
+//  XcodeBuilder2
+//
+//  Created by Kai Shao on 2025/7/14.
+//
+
+import SwiftUI
+
+struct ProjectDetailView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
